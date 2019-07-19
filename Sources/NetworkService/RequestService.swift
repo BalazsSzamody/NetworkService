fb3505bea398ccdc87c2d44fcb6886b26eb1dbd0
Unknown <bszamody@gmail.com>
2019-07-20 09:40:09 +1000

    Initial commit

---
+++ Sources/NetworkService/RequestService.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Balazs Szamody on 20/7/19.
+//
+
+import Foundation
+
+protocol RequestServicing {
+    
+}
+
+struct RequestService: RequestServicing {
+    public enum Method
+}
